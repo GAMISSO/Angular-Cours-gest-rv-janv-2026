5990143a382ff1deceaf51a432116e71f83f91d6
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2026-02-24 14:18:33 +0100

    Validation FormModule in Templates

--- src/app/core/guards/is-connect-guard.ts
+++ src/app/core/guards/is-connect-guard.ts
@@ -3,9 +3,9 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Security } from '../services/security';
 
 export const isConnectGuard: CanActivateFn = (route, state) => {
-  let securityService:Security = inject(Security);
+  let securityService: Security = inject(Security);
   let router = inject(Router);
-  if (securityService.isAuthenticated()) {
+  if (!securityService.isAuthenticated()) {
     router.navigate(['/public/login']);
     return false;
   }
--- src/app/features/public/login/login.ts
+++ src/app/features/public/login/login.ts
@@ -1,13 +1,13 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserLoginRequest } from 'src/app/core/models/user.model';
 import { Security } from 'src/app/core/services/security';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,JsonPipe,CommonModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -17,19 +17,23 @@ export class Login {
     password: ''
   };
 
+  errorMessage: string = '';
   nom: string = 'John DOE';
   constructor(private securityService: Security,private router: Router) {}
 
-  onLogin(): void {
+  onLogin(loginForm: NgForm): void {
+    if (loginForm.invalid) {
+      this.errorMessage = 'Veuillez remplir tous les champs requis.';
+      return;
+    }
     // Logique de connexion ici
     const loginResult = this.securityService.login(this.userLogin);
     if (loginResult != null) {
       // Connexion réussie
       this.router.navigate(['/private/dash']);
-    } else {
-      // Échec de la connexion
-      console.log('Échec de la connexion');
-    }
+    } 
   }
+
+  
 
 }
